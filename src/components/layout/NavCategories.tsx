1b656ad589b0f4d3e92ce551e21d2c41f0a0eba9
Caraje <carlosajenjo@gmail.com>
2024-01-04 13:08:00 +0100

    create component for NavCategories

---
+++ src/components/layout/NavCategories.tsx
@@ -0,0 +1,32 @@
+"use client"
+import Link from 'next/link'
+import styles from '@/styles/layout/NavCategories.module.css'
+import { usePathname } from 'next/navigation'
+
+
+export const NavCategories = () => {
+  const pathname = usePathname()
+  console.log(pathname)
+  return (
+    <nav className={styles.nav_categories}>
+      <Link 
+        href='/movies'
+        className={(pathname === '/movies') ? styles.cat_link_active : styles.cat_link}
+      >
+          Peliculas
+      </Link>
+      <Link 
+        href='/series'
+        className={(pathname === '/series') ? styles.cat_link_active : styles.cat_link}
+      >
+          Series
+      </Link>
+      <Link 
+        href='/videogames'
+        className={(pathname === '/videogames') ? styles.cat_link_active : styles.cat_link}
+      >
+          Videojuegos
+      </Link>
+    </nav>
+  )
+}
