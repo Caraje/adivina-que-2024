f0b60e369ae440f8fed37da20b36af5c5a9189d1
Caraje <carlosajenjo@gmail.com>
2024-01-08 19:49:23 +0100

    create UserCard

---
+++ src/components/categories/UserCard.tsx
@@ -0,0 +1,35 @@
+import styles from '@/styles/catagories/UserCard.module.css'
+import { exampleUser } from '@/utils/exampleActiveUser'
+import { IconError, IconRight, IconScore } from '../Icons'
+
+export const UserCard = () => {
+  exampleUser
+  return (
+    <article className={styles.card}>
+      <img 
+        src={exampleUser.user_avatar}  
+        alt={`Imagen del avatar de ${exampleUser.user_name}`}
+        width={150}
+        height={150}
+        className={styles.avatar}
+      />
+      <div className={styles.user_info}>
+        <h2 className={styles.user_title}>Puntuación</h2>
+        <section className={styles.score_board}>
+          <article className={styles.score_points}>
+            <IconScore size={30}/>
+            <p>999</p>      
+          </article>
+          <article className={styles.score_correct}>
+            <IconRight size={30} />
+            <p>999</p>      
+          </article>
+          <article className={styles.score_error}>
+            <IconError size={30} />
+            <p>999</p>      
+          </article>
+        </section>
+      </div>
+    </article>
+  )
+}
