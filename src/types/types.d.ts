e5c6701c990aa668afe9f960cc676b99274a9b46
Caraje <carlosajenjo@gmail.com>
2024-01-08 19:47:16 +0100

    add types for user

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -6,3 +6,30 @@ export type RankList = {
   avatar: string, 
   points: number
 }
+
+type Networks = {
+  network_id: string,
+  network_name: string, 
+  network_url: string
+}
+
+type Level = {
+  level_id: string, 
+  level_correct: string, 
+  level_errors: number, 
+  level_points: number, 
+}
+type DataGame = {
+  movies: Level[],
+  series: Level[],
+  videogames: Level[]
+}
+export type User = {
+  user_id: string,
+  user_name: string,
+  user_email: string,
+  user_avatar: string,
+  user_created: string,
+  user_networks: Networks[],
+  user_datagame: DataGame,
+}
