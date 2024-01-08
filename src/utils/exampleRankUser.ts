f900d464988958a691dffae2d7b5726afe979953
Caraje <carlosajenjo@gmail.com>
2024-01-08 17:03:44 +0100

    create rankList for ranks examples

---
+++ src/utils/exampleRankUser.ts
@@ -0,0 +1,64 @@
+import { RankList } from "@/types/types";
+
+export const rankList: RankList[] = [
+  {
+    id: '1',
+    name: 'PaladinHup',
+    avatar: './static_img/avatars/01.jpg', 
+    points: 999
+  }, 
+  {
+    id: '2',
+    name: 'Caraje',
+    avatar: './static_img/avatars/02.jpg', 
+    points: 999
+  }, 
+  {
+    id: '3',
+    name: 'Aremi',
+    avatar: './static_img/avatars/03.jpg', 
+    points: 999
+  },
+  {
+    id: '4',
+    name: 'celirgah',
+    avatar: './static_img/avatars/04.jpg', 
+    points: 999
+  }, 
+  {
+    id: '5',
+    name: 'titanica',
+    avatar: './static_img/avatars/05.jpg', 
+    points: 999
+  }, 
+  {
+    id: '6',
+    name: 'elmo',
+    avatar: './static_img/avatars/06.webp', 
+    points: 999
+  },
+  {
+    id: '7',
+    name: 'puttydutty',
+    avatar: './static_img/avatars/07.jpg', 
+    points: 999
+  }, 
+  {
+    id: '8',
+    name: 'wisy777',
+    avatar: './static_img/avatars/08.jpg', 
+    points: 999
+  }, 
+  {
+    id: '9',
+    name: 'kaisserTF',
+    avatar: './static_img/avatars/09.gif', 
+    points: 999
+  },
+  {
+    id: '10',
+    name: 'Midudev',
+    avatar: './static_img/avatars/10.jpg', 
+    points: 999
+  },
+]
