0ddf6e3c77f577f34f129856bf3fb485ebe8d23e
Caraje <carlosajenjo@gmail.com>
2024-01-08 19:49:56 +0100

    create AsideSection

---
+++ src/components/categories/AsideSection.tsx
@@ -0,0 +1,21 @@
+import styles from '@/styles/catagories/AsideSection.module.css'
+import { RankCard } from '../Home/RankCard'
+import { rankList } from '@/utils/exampleRankUser'
+import { UserCard } from './UserCard'
+
+interface Props {
+  cat: string
+}
+
+const AsideSection: React.FC<Props> = ({cat}) => {
+  return (
+    <section className={styles.container}>
+        <UserCard />
+      <section className={styles.rankBoard}> 
+        <RankCard cat={cat} list={rankList} isGame/>
+      </section>
+    </section>
+  )
+}
+
+export default AsideSection
