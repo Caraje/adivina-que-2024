a9c694e91bda429d589bc39433a64bafb15dbc30
Caraje <carlosajenjo@gmail.com>
2024-01-08 19:49:48 +0100

    create GameSection

---
+++ src/components/categories/GameSection.tsx
@@ -0,0 +1,8 @@
+import styles from '@/styles/catagories/GameSection.module.css'
+export const GameSection = () => {
+  return (
+    <section className={styles.container}>
+      aqui el juego
+    </section>
+  )
+}
