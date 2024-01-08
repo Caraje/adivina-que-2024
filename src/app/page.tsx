320fce568236bf2a80bf75c05a3772563d0b58af
Caraje <carlosajenjo@gmail.com>
2024-01-08 17:06:39 +0100

    add SelectCategory and AsideRankings to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
-
-export default function Home() {
+import AsideRankings from '@/components/Home/AsideRankings'
+import { SelectCategory } from '@/components/Home/SelectCategory'
+import styles from '@/styles/Home/homePage.module.css'
+export default function HomePage() {
   return (
-    <h1>Adivina que</h1>
+    <div className={styles.container}>
+      <SelectCategory />
+      <AsideRankings />
+    </div>
   )
 }
