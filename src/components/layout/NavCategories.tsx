7903137f95581557f665626593d34333a28d8865
Caraje <carlosajenjo@gmail.com>
2024-01-08 17:05:17 +0100

    delete console log

--- src/components/layout/NavCategories.tsx
+++ src/components/layout/NavCategories.tsx
@@ -6,7 +6,6 @@ import { usePathname } from 'next/navigation'
 
 export const NavCategories = () => {
   const pathname = usePathname()
-  console.log(pathname)
   return (
     <nav className={styles.nav_categories}>
       <Link 
