12b2a84fa21781186b6a15267075ae4d553c0f26
Caraje <carlosajenjo@gmail.com>
2024-01-08 19:49:37 +0100

    create GamePage

---
+++ src/components/categories/GamePage.tsx
@@ -0,0 +1,16 @@
+import styles from '@/styles/catagories/GamePage.module.css'
+import AsideSection from './AsideSection'
+import { GameSection } from './GameSection'
+
+interface Props {
+  cat: string
+}
+
+export const GamePage: React.FC<Props> = ({cat}) => {
+  return (
+    <div className={styles.container}>
+      <GameSection />
+      <AsideSection cat={cat}/>
+    </div>
+  )
+}
