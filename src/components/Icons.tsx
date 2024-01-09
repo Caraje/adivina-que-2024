95e050404b351e2c62738a74c8c30a8512b95ec7
Caraje <carlosajenjo@gmail.com>
2024-01-09 20:09:34 +0100

    fox error in icon

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -21,7 +21,7 @@ export const IconScore: React.FC<Props> = ({ size = 50 }) => {
 export const IconError: React.FC<Props> = ({ size = 50 }) => {
   return (
 <svg width={size} height={size} viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-    <path fill="currentColor" fill-rule="evenodd" d="M8 14.5a6.5 6.5 0 1 0 0-13a6.5 6.5 0 0 0 0 13M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m1-5a1 1 0 1 1-2 0a1 1 0 0 1 2 0m-.25-6.25a.75.75 0 0 0-1.5 0v3.5a.75.75 0 0 0 1.5 0z" clip-rule="evenodd"/>
+    <path fill="currentColor" fillRule="evenodd" d="M8 14.5a6.5 6.5 0 1 0 0-13a6.5 6.5 0 0 0 0 13M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m1-5a1 1 0 1 1-2 0a1 1 0 0 1 2 0m-.25-6.25a.75.75 0 0 0-1.5 0v3.5a.75.75 0 0 0 1.5 0z" clipRule="evenodd"/>
 </svg>
   )
 }
