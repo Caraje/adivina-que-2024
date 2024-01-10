f2d247387bcfdd7c16b966291ac9e70cee7cfdbe
Caraje <carlosajenjo@gmail.com>
2024-01-10 19:00:46 +0100

    fix code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import { SelectCategory } from '@/components/Home/SelectCategory'
 import styles from '@/styles/Home/homePage.module.css'
 export default function HomePage() {
   return (
-    <div className={styles.container}>
-      <SelectCategory />
-      <AsideRankings />
-    </div>
+    <>
+      <div className={styles.container}>
+        <SelectCategory />
+        <AsideRankings />
+      </div>
+    </>
   )
 }
