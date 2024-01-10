1cbfea6dbedb5be26c5aa905c289d003c799a030
Caraje <carlosajenjo@gmail.com>
2024-01-10 19:00:34 +0100

    add funcitionalities to button

--- src/components/layout/UserButton.tsx
+++ src/components/layout/UserButton.tsx
@@ -1,12 +1,18 @@
+'use client'
 import styles from '@/styles/layout/UserButton.module.css'
 import { IconUser } from '../Icons'
+import { useModalStore } from '@/store/modal-store'
 
 
 export const UserButton = () => {
+  const { toggleModal } = useModalStore()
+
   return (
     <section className={styles.user_menu}>
     {/* TODO: Crear botones para ir a registro/login y boton para abrir menu de usuario */}
-    Login/Register <IconUser size={30} />
+    <button onClick={toggleModal}>
+      Login/Register <IconUser size={30} />
+    </button>
   </section>
   )
 }
