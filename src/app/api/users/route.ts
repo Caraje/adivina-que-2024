02859fae42627e19d2d7eb9a8dd2ea691f87eab4
Caraje <carlosajenjo@gmail.com>
2024-01-11 19:13:45 +0100

    create handlers for create and get users

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,10 +1,32 @@
+import { randomUUID } from 'crypto'
+import { NextResponse } from "next/server"
+import { connectDB } from "@/config/db"
+import User from "@/models/User"
 
 
-export async function GET(req: Request) {
-  return new Response('hola desde users GET')
+export async function GET() {
+  connectDB()
+const test = randomUUID()
+  const user = await User.find()
+  return NextResponse.json({
+    message: 'hola desde users GET',
+    user,
+    test
+  })
 }
-export async function POST(req: Request) {
-  return new Response('hola desde users POST')
+export async function POST(request: Request) {
+  try {
+    const data = await request.json()
+    const newUser = new User(data)
+    console.log({newUser})
+    const savedUser = await newUser.save()
+    return NextResponse.json(savedUser, newUser)    
+  } catch (error) {
+    return NextResponse.json(error,{
+      status: 400
+    })
+    
+  }
 }
 export async function PATCH(req: Request) {
   return new Response('hola desde users PATCH')
