a677b2d2b6c7f8267c944d03105d828f20a940e2
Caraje <carlosajenjo@gmail.com>
2024-01-11 19:14:02 +0100

    create handlers

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,19 +1,15 @@
+import { connectDB } from "@/config/db"
+
+import { NextResponse } from "next/server"
 
 
-export async function GET(req: Request) {
-  const testResponse = {
-    "status": "202", 
-    "message": "Hola desde categoriresGET"
-  }
-  const test = JSON.stringify(testResponse)
-  console.log({test})
-  return new Response(JSON.stringify({
-    "status": "202", 
-    "message": "Hola desde categoriresGET"
-  }))
+export async function GET() {
+  return new Response('hola desde categories GET')
+
 }
-export async function POST(req: Request) {
+export async function POST() {
   return new Response('hola desde categories POST')
+
 }
 export async function PATCH(req: Request) {
   return new Response('hola desde categories PATCH')
