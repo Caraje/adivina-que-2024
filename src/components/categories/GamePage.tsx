d517aae8a40dfe2b2504be41e29696125741eefb
Caraje <carlosajenjo@gmail.com>
2024-01-22 21:38:29 +0100

    add useSession

--- src/components/categories/GamePage.tsx
+++ src/components/categories/GamePage.tsx
@@ -1,12 +1,17 @@
+'use client'
 import styles from '@/styles/catagories/GamePage.module.css'
 import {AsideSection} from './AsideSection'
 import { GameSection } from './GameSection'
+import { useSession } from 'next-auth/react'
 
 interface Props {
   cat: string
 }
 
 export const GamePage: React.FC<Props> = ({cat}) => {
+  const { data, status } = useSession()
+
+  console.log({data, status})
   return (
     <div className={styles.container}>
       <GameSection />
