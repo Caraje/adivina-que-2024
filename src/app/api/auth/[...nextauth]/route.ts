60cb229df0616bc82cb3f0a55d4425069f4d265d
Caraje <carlosajenjo@gmail.com>
2024-01-23 17:50:54 +0100

    fix return user data

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -9,30 +9,32 @@ const handler = NextAuth({
     CredentialsProvider({
       name: 'credentials',
       credentials: {
-        user_email: { label: "user_email", type: "text", placeholder: "jsmith" },
+        email: { label: "user_email", type: "text", placeholder: "jsmith" },
         user_password: { label: "Password", type: "password" }
       },
       async authorize(credentials, req) {
         await connectDB()
-        const userFound = await User.findOne({user_email: credentials?.user_email})
+        const userFound = await User.findOne({user_email: credentials?.email})
         if(!userFound) throw new Error('Invalid Credentials')
         const passMatch = await bcrypt.compare(credentials!.user_password, userFound.user_password)
         if(!passMatch) throw new Error('invalid Credentials')
-        if (userFound) return userFound.
-        return 
+        return userFound
       },
     })
   ],
   callbacks: {
     jwt({account, token, user, profile, session}) {
-      // console.log({account, token, user, profile, session})
+      if (user) token.user = user;
       return token
     },
-    session({session, token}) {
-      console.log({session, token})
+    session({session, token, user}) {
+      session.user = token.user as any;
+      console.log({session, token, user})
       return session
     }
   }
 })
 
 export { handler as GET, handler as POST }
+
+
