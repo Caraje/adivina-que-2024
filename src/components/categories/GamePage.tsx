cc428bcda5411f1ebaa8e366244af5d5fb0ab023
Caraje <carlosajenjo@gmail.com>
2024-01-23 17:51:24 +0100

    delete useSession

--- src/components/categories/GamePage.tsx
+++ src/components/categories/GamePage.tsx
@@ -2,16 +2,16 @@
 import styles from '@/styles/catagories/GamePage.module.css'
 import {AsideSection} from './AsideSection'
 import { GameSection } from './GameSection'
-import { useSession } from 'next-auth/react'
+// import { useSession } from 'next-auth/react'
 
 interface Props {
   cat: string
 }
 
 export const GamePage: React.FC<Props> = ({cat}) => {
-  const { data, status } = useSession()
+  // const { data, status } = useSession()
 
-  console.log({data, status})
+  // console.log({data, status})
   return (
     <div className={styles.container}>
       <GameSection />
