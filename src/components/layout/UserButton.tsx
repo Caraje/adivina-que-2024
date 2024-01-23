5a2bfc1f2e36e9bf711f698ccbefa42a2956f959
Caraje <carlosajenjo@gmail.com>
2024-01-23 17:51:59 +0100

    add temporal button for signin or signout

--- src/components/layout/UserButton.tsx
+++ src/components/layout/UserButton.tsx
@@ -2,11 +2,15 @@
 import styles from '@/styles/layout/UserButton.module.css'
 import { IconUser } from '../Icons'
 import { useModalStore } from '@/store/modal-store'
+import { useSession } from 'next-auth/react'
+import Link from 'next/link'
 
 
 export const UserButton = () => {
   const { toggleModal } = useModalStore()
+  const { data, status } = useSession()
 
+  console.log({data, status})
   return (
     <section className={styles.user_menu}>
     {/* TODO: Crear botones para ir a registro/login y boton para abrir menu de usuario */}
@@ -15,7 +19,13 @@ export const UserButton = () => {
       className={styles.btn_user}
     >
       Login/Register <IconUser size={30} />
-    </button>
+    </button> 
+    {
+      (status === "authenticated") 
+        ? <Link href='/api/auth/signout'>signout</Link>
+        : <Link href='/api/auth/signin'>SignIn</Link>
+    }
+
   </section>
   )
 }
