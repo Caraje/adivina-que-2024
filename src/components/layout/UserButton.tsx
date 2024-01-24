b2e952bb6cfcc3dfc05cc4ae7c39368ba3fcb4e8
Caraje <carlosajenjo@gmail.com>
2024-01-24 12:35:25 +0100

    add button logout (temporaly)

--- src/components/layout/UserButton.tsx
+++ src/components/layout/UserButton.tsx
@@ -3,13 +3,17 @@ import styles from '@/styles/layout/UserButton.module.css'
 import { IconUser } from '../Icons'
 import { useModalStore } from '@/store/modal-store'
 import { useSession } from 'next-auth/react'
+import { signOut } from "next-auth/react"
 import Link from 'next/link'
 
 
 export const UserButton = () => {
   const { toggleModal } = useModalStore()
   const { data, status } = useSession()
-
+  
+  const handleLogout = async () => {
+    signOut()
+  }
   console.log({data, status})
   return (
     <section className={styles.user_menu}>
@@ -20,11 +24,22 @@ export const UserButton = () => {
     >
       Login/Register <IconUser size={30} />
     </button> 
-    {
+    {/* {
       (status === "authenticated") 
         ? <Link href='/api/auth/signout'>signout</Link>
         : <Link href='/api/auth/signin'>SignIn</Link>
+    } */}
+    {
+      (status === "authenticated") &&
+        (
+        <button
+          onClick={handleLogout}
+        >
+          logout
+        </button>
+        )
     }
+
 
   </section>
   )
