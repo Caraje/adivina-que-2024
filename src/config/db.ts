a2aaf8edfa4c995f7831e32e894c5bf07e6dd4a6
Caraje <carlosajenjo@gmail.com>
2024-01-24 18:28:01 +0100

    fix variables

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,12 +1,13 @@
-import { URL_BASE } from '@/utils/env';
+// import { URL_BASE } from '@/utils/env';
 import mongoose from 'mongoose'
 
-if(URL_BASE === '') {
+const MONGODB_URI: string = process.env.MONGODB_URI || ''
+if(process.env.MONGODB_URI === '') {
   throw new Error('No esta definida la URL de Mongo')
 }
 export const connectDB = async () => {
   try {
-    const { connection} = await mongoose.connect(URL_BASE)
+    const { connection} = await mongoose.connect(MONGODB_URI)
     if(connection.readyState === 1){
       console.log('MongoDB Connected');
       return Promise.resolve(true)
