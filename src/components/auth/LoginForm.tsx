5a02e7f5115015b3818a649ac45d554f94adc3da
Caraje <carlosajenjo@gmail.com>
2024-01-24 12:34:51 +0100

    add funtionality for login form

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,11 +1,23 @@
 import { useModalStore } from '@/store/modal-store'
 import styles from '@/styles/modalLogin/LoginForm.module.css'
+import { CreateUser } from '@/types/types'
+import { signIn } from 'next-auth/react'
 
 interface Props {
   toRegister: React.Dispatch<React.SetStateAction<boolean>>
 }
 export const LoginForm: React.FC<Props> = ({ toRegister }) => {
   const { toggleModal } = useModalStore()
+  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const form = event.currentTarget
+    const formData = new FormData(form)
+    const userData = Object.fromEntries(formData.entries()) as CreateUser;
+    const res = await signIn('credentials', {
+      email: userData.user_email,
+      password: userData.user_password
+    })
+  }
   return (
     <section className={styles.container}>
       <picture className={styles.picture_container}>
@@ -24,17 +36,33 @@ export const LoginForm: React.FC<Props> = ({ toRegister }) => {
         >X</button>
         <header className={styles.form_container}>
           <h2 className={styles.title}>Login</h2>
-          <form className={styles.form}>
+          <form 
+            className={styles.form}
+            onSubmit={handleLogin}
+          >
             <label className={styles.form_label}>
-              Nombre de usuario: 
-              <input className={styles.form_input}/>
+            Email: 
+            <input 
+                className={styles.form_input}
+                type="email"
+                name='user_email'
+                placeholder="email@email.com"
+                autoComplete="off"
+                />
             </label>
             <label className={styles.form_label}>
               password: 
-              <input className={styles.form_input}/>
+              <input 
+                className={styles.form_input}
+                type="password"
+                name='user_password'
+                placeholder="Password"
+                autoComplete="off"
+                />
             </label>
           <button>Enviar</button>
           </form>
+          <button onClick={() => signIn("github")}>Sign in with Github</button>
         </header>
 
         <div className={styles.footer}>
