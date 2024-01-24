63a35c766e780a6219db461caef8df8a9f1a7964
Caraje <carlosajenjo@gmail.com>
2024-01-24 12:33:43 +0100

    create env file

---
+++ src/utils/env.d.ts
@@ -0,0 +1,15 @@
+
+
+// Variables Github
+export const GITHUB_ID: string = process.env.GITHUB_ID
+export const GITHUB_CLIENT: string = process.env.GITHUB_SECRET
+
+// Variables Mongo
+export const URL_BASE: string= process.env.MONGODB_URI
+
+// Variables generals
+export const NEXT_PUBLIC_URL_BASE: string = process.env.NEXT_PUBLIC_URL_BASE
+
+// Variables NEXTAUTH
+export const NEXTAUTH_URL: string= process.env.NEXTAUTH_URL
+export const NEXTAUTH_SECRET: string= process.env.NEXTAUTH_SECRET
