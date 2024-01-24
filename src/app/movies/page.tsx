05badc6c64dfb0e8015954f22c64005642d193c5
Caraje <carlosajenjo@gmail.com>
2024-01-24 20:45:24 +0100

    add levelByCategory function

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,22 +1,23 @@
 import { GamePage } from "@/components/categories/GamePage";
 
-// export const levelByCategory = async (cat) => {
-//   try {
-//     const response = await fetch(`http://localhost:3000/api/levels/movies`, )
-//     if (response.ok) {
-//       const {levelList} = await response.json()
-//       return levelList
-//     }
-//   } catch (error) {
-//     console.log(error)
-//     return { error: 'Conection Error' }
-//   }
-// }
+const levelByCategory = async (cat: string) => {
+  try {
+    const response = await fetch(`http://localhost:3000/api/levels/${cat}`, )
+    if (response.ok) {
+      const {levelList} = await response.json()
+      return levelList
+    }
+  } catch (error) {
+    console.log(error)
+    return { error: 'Conection Error' }
+  }
+}
 
 export default async function MoviesPage() {
-// const levelList = await levelByCategory('movies') 
-// console.log(levelList)
+
+  const levelList = await levelByCategory('movies')
+  console.log(levelList)
   return (
-    <GamePage cat={'movies'} />
+    <GamePage cat={'movies'} lvl={levelList}/>
   )
 }
