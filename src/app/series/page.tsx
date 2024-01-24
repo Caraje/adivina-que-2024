707c8cc559545e3583079da0fbf9c82c38f2d94e
Caraje <carlosajenjo@gmail.com>
2024-01-24 20:45:36 +0100

    add levelBycategory function

--- src/app/series/page.tsx
+++ src/app/series/page.tsx
@@ -1,8 +1,21 @@
 import { GamePage } from "@/components/categories/GamePage";
 
-
-export default function SeriesPage() {
+const levelByCategory = async (cat: string) => {
+  try {
+    const response = await fetch(`http://localhost:3000/api/levels/${cat}`, )
+    if (response.ok) {
+      const {levelList} = await response.json()
+      return levelList
+    }
+  } catch (error) {
+    console.log(error)
+    return { error: 'Conection Error' }
+  }
+}
+export default async function SeriesPage() {
+  const levelList = await levelByCategory('series')
+  console.log(levelList)
   return (
-    <GamePage cat={'series'} />
+    <GamePage cat={'series'} lvl={levelList}/>
   )
 }
