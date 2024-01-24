90bed8aa681cdee1447ac26c608dc98071439f3b
Caraje <carlosajenjo@gmail.com>
2024-01-24 18:29:08 +0100

    change name cat

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,9 +1,22 @@
 import { GamePage } from "@/components/categories/GamePage";
 
+// export const levelByCategory = async (cat) => {
+//   try {
+//     const response = await fetch(`http://localhost:3000/api/levels/movies`, )
+//     if (response.ok) {
+//       const {levelList} = await response.json()
+//       return levelList
+//     }
+//   } catch (error) {
+//     console.log(error)
+//     return { error: 'Conection Error' }
+//   }
+// }
 
-
-export default function MoviesPage() {
+export default async function MoviesPage() {
+// const levelList = await levelByCategory('movies') 
+// console.log(levelList)
   return (
-    <GamePage cat={'cine'} />
+    <GamePage cat={'movies'} />
   )
 }
