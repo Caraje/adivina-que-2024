2e17a37a927aa8268f61d17705693d112b82087c
Caraje <carlosajenjo@gmail.com>
2024-01-24 18:30:00 +0100

    add get level list form database

--- src/components/categories/GamePage.tsx
+++ src/components/categories/GamePage.tsx
@@ -1,21 +1,43 @@
-'use client'
+
 import styles from '@/styles/catagories/GamePage.module.css'
 import {AsideSection} from './AsideSection'
 import { GameSection } from './GameSection'
-// import { useSession } from 'next-auth/react'
 
+interface iCategories {
+  movies: string, 
+  series: string, 
+  videogames: string
+}
 interface Props {
-  cat: string
+  cat: keyof iCategories
 }
 
-export const GamePage: React.FC<Props> = ({cat}) => {
-  // const { data, status } = useSession()
+const CategoriesName = {
+  movies: 'Cine', 
+  series: 'Series', 
+  videogames: 'Videojuegos'
+}
 
-  // console.log({data, status})
+const levelByCategory = async (cat) => {
+    try {
+      const response = await fetch(`http://localhost:3000/api/levels/${cat}`, )
+      if (response.ok) {
+        const {levelList} = await response.json()
+        return levelList
+      }
+    } catch (error) {
+      console.log(error)
+      return { error: 'Conection Error' }
+    }
+  }
+
+export const GamePage: React.FC<Props> = async ({cat}) => {
+const levelList = await levelByCategory(cat)
+console.log({levelList})
   return (
     <div className={styles.container}>
-      <GameSection />
-      <AsideSection cat={cat}/>
+      <GameSection cat={cat}/>
+      <AsideSection cat={CategoriesName[cat]}/>
     </div>
   )
 }
