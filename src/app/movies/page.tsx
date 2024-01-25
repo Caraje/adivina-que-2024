6e62719312bfd27b8adbbb75f29798f7b2751571
Caraje <carlosajenjo@gmail.com>
2024-01-25 16:20:51 +0100

    replace logic with function from file

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,22 +1,9 @@
 import { GamePage } from "@/components/categories/GamePage";
-
-const levelByCategory = async (cat: string) => {
-  try {
-    const response = await fetch(`http://localhost:3000/api/levels/${cat}`, )
-    if (response.ok) {
-      const {levelList} = await response.json()
-      return levelList
-    }
-  } catch (error) {
-    console.log(error)
-    return { error: 'Conection Error' }
-  }
-}
+import { levelByCategory } from "@/controllers/levels";
 
 export default async function MoviesPage() {
 
   const levelList = await levelByCategory('movies')
-  console.log(levelList)
   return (
     <GamePage cat={'movies'} lvl={levelList}/>
   )
