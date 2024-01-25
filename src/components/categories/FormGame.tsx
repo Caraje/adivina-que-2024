21cac32f308f39b14c9ceff7609d63a0a4899a35
Caraje <carlosajenjo@gmail.com>
2024-01-25 16:20:24 +0100

    add value and onChange to input from state

--- src/components/categories/FormGame.tsx
+++ src/components/categories/FormGame.tsx
@@ -4,14 +4,24 @@ import { handleNextClue } from '@/types/types';
 interface Props {
   isCorrect: boolean; 
   isIncorrect: boolean;
-  handleNextClue: handleNextClue
+  handleNextClue: handleNextClue;
+  lvlAnswer: string, 
+  setLvlAnswer: React.Dispatch<React.SetStateAction<string>>,
+  handleAnswerForm: (e: React.FormEvent<HTMLFormElement>) => void
 
 }
 
-export const FormGame: React.FC<Props> = ({isCorrect, isIncorrect, handleNextClue}) => {
+export const FormGame: React.FC<Props> = ({isCorrect, isIncorrect, handleNextClue, lvlAnswer,
+  setLvlAnswer, handleAnswerForm}) => {
+    
   return (
-    <form className={styles.container}>
-      <input className={styles.form_input}/>
+    <form className={styles.container} onSubmit={(e) => {handleAnswerForm(e)}}>
+      <input 
+        className={styles.form_input}
+        type='text'
+        value={lvlAnswer}
+        onChange={(e) => {setLvlAnswer(e.target.value)}}
+      />
       <div className={styles.button_group}>
         {
           (!isCorrect && !isIncorrect) &&
