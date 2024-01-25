27e4019cbe11bc50d027484bed8288d95bf4fb19
Caraje <carlosajenjo@gmail.com>
2024-01-25 18:11:16 +0100

    add info from database

--- src/components/categories/UserCard.tsx
+++ src/components/categories/UserCard.tsx
@@ -1,31 +1,43 @@
+'use client'
 import styles from '@/styles/catagories/UserCard.module.css'
-import { exampleUser } from '@/utils/exampleActiveUser'
 import { IconError, IconRight, IconScore } from '../Icons'
+import { useSession } from 'next-auth/react'
+
 
 export const UserCard = () => {
+  const { data } = useSession()
+  const user: any = data?.user;
+  const errorFilter = user?.user_datagame.movies.filter((er:any) => !er.level_correct ) || []
+  const sucessFilter = user?.user_datagame.movies.filter((er:any) => er.level_correct ) || []
+  let totalPoints = 0
+  for (let i = 0; i < sucessFilter?.length; i++) { 
+    totalPoints += sucessFilter[i].level_points
+  }
+
+
   return (
     <article className={styles.card}>
       <img 
-        src={exampleUser.user_avatar}  
-        alt={`Imagen del avatar de ${exampleUser.user_name}`}
+        src={user?.user_avatar}  
+        alt={`Imagen del avatar de ${user?.user_name}`}
         width={150}
         height={150}
         className={styles.avatar}
       />
       <div className={styles.user_info}>
-        <h2 className={styles.user_title}>Puntuación</h2>
+        <h2 className={styles.user_title}>{user?.user_name}</h2>
         <section className={styles.score_board}>
           <article className={styles.score_points}>
             <IconScore size={30}/>
-            <p>999</p>      
+            <p>{totalPoints}</p>      
           </article>
           <article className={styles.score_correct}>
             <IconRight size={30} />
-            <p>999</p>      
+            <p>{sucessFilter?.length}</p>      
           </article>
           <article className={styles.score_error}>
             <IconError size={30} />
-            <p>999</p>      
+            <p>{errorFilter?.length}</p>      
           </article>
         </section>
       </div>
