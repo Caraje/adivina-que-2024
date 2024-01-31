c272c807c2002b48478c465c7c861a8edf46e563
Caraje <carlosajenjo@gmail.com>
2024-01-31 14:30:53 +0100

    clean code

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -2,9 +2,8 @@ import { GamePage } from "@/components/categories/GamePage";
 import { levelByCategory } from "@/controllers/levels";
 
 export default async function MoviesPage() {
-  
-
   const levelList = await levelByCategory('movies')
+  
   return (
     <GamePage cat={'movies'} lvl={levelList}/>
   )
