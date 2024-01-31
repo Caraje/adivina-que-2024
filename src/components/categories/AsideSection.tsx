f7858a099cc429d937a3129a7aa6deb4bcc6e4ce
Caraje <carlosajenjo@gmail.com>
2024-01-31 14:30:45 +0100

    fix prop cat

--- src/components/categories/AsideSection.tsx
+++ src/components/categories/AsideSection.tsx
@@ -7,10 +7,12 @@ interface Props {
   cat: string
 }
 
+
+
 export const AsideSection: React.FC<Props> = ({cat}) => {
   return (
     <section className={styles.container}>
-        <UserCard />
+        <UserCard cat={cat}/>
       <section className={styles.rankBoard}> 
         <RankCard cat={cat} list={rankList} isGame/>
       </section>
