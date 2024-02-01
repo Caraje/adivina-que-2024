577126c2f9e9823740cb815e8f23274c4ab7c968
Caraje <carlosajenjo@gmail.com>
2024-02-01 14:17:20 +0100

    add userPageSection

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,9 +1,9 @@
-import { exampleUser } from "@/utils/exampleActiveUser"
+import { UserPageSection } from "@/components/user/UserPageSection"
 
 export default function UserPage({params}: { params: { id: string }}) {
   const { id } = params
-  const user = exampleUser
+
   return (
-    <h1>Esto es la pagina de usuarios</h1>
+    <UserPageSection id={id}/>
   )
 }
