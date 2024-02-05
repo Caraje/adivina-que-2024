1b83734be9678b9ab2a1f5171aea2a5589a629e9
Caraje <carlosajenjo@gmail.com>
2024-02-05 16:44:40 +0100

    add card user

--- src/components/user/UserPageSection.tsx
+++ src/components/user/UserPageSection.tsx
@@ -2,31 +2,53 @@
 import useUserData from '@/hooks/user'
 import { useSession } from 'next-auth/react'
 import React from 'react'
+import { UserDataCard }  from '@/components/user/UserDataCard'
+import { ScoreCategoryCard } from './ScoreCategoryCard'
+import styles from '@/styles/user/UserPageSection.module.css'
+import { User } from '@/types/types'
 
 interface Props {
   id: string
 }
 export const UserPageSection: React.FC<Props> = ({id}) => {
   const { data, status, update } = useSession()
-  const userPage = useUserData(id)
+  const userPage: any = useUserData(id) 
   const user: any = data?.user
   if (status === 'loading') {
     return <div>Cargando...</div>
   }
   if(user?.user_id === id) {
     return (
-      <section>
-        <section>
-          Aqui la card del usuario
+      <section className={styles.container}>
+        <section className={styles.user_section}>
+          <UserDataCard userData={userPage}/>          
         </section>
-        <section>
-          Aqui seccion con cards de puntuacion de las categorias
+        <section className={styles.score_section}>
+          <header className={styles.score_title}>Puntuación</header>
+          <section className={styles.score_cards_group}>
+            <ScoreCategoryCard cat={'movies'} info={userPage?.user_datagame['movies']}/>
+            <ScoreCategoryCard cat={'series'} info={userPage?.user_datagame['series']}/>
+            <ScoreCategoryCard cat={'videogames'} info={userPage?.user_datagame['videogames']}/>
+          </section>
           {/* Seran cards por categorias, crear una para pasarle las diferentes categorias http://localhost:3000/user/5a1f3a30-ef56-4cb6-bbfe-7bcd6569d92e*/}
         </section>
       </section>
     )
   }
   return (
-    <div>Es otro usuario</div>
+    <section className={styles.container}>
+        <section className={styles.user_section}>
+          <UserDataCard userData={userPage}/>          
+        </section>
+        <section className={styles.score_section}>
+          <header className={styles.score_title}>Puntuación</header>
+          <section className={styles.score_cards_group}>
+            <ScoreCategoryCard cat={'movies'} info={userPage?.user_datagame['movies']}/>
+            <ScoreCategoryCard cat={'series'} info={userPage?.user_datagame['series']}/>
+            <ScoreCategoryCard cat={'videogames'} info={userPage?.user_datagame['videogames']}/>
+          </section>
+          {/* Seran cards por categorias, crear una para pasarle las diferentes categorias http://localhost:3000/user/5a1f3a30-ef56-4cb6-bbfe-7bcd6569d92e*/}
+        </section>
+      </section>
   )
 }
