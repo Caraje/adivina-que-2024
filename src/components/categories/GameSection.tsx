f928ee30d113eac722f67d5732e0e41734e6c778
Caraje <carlosajenjo@gmail.com>
2024-02-06 16:18:28 +0100

    rafactor code

--- src/components/categories/GameSection.tsx
+++ src/components/categories/GameSection.tsx
@@ -1,17 +1,10 @@
 
 'use client'
-import styles from '@/styles/catagories/GameSection.module.css'
-import { CanvasGame } from './CanvasGame'
-import { PositionLevel } from './PositionLevel'
-import { useState } from 'react'
-import { FormGame } from './FormGame'
-import { ClueGame } from './ClueGame'
 import { LevelGame } from '@/types/types'
 import { useSession } from 'next-auth/react'
 import useUserData from '@/hooks/user'
-import { UpdateUserById } from '@/controllers/users'
-
-
+import { DemoGame } from './DemoGame'
+import { LoggedUserGame } from './LoggedUserGame'
 
 interface Props {
   cat: string,
@@ -21,14 +14,10 @@ interface Props {
 
 export const GameSection: React.FC<Props> = ({cat, lvl}) => {
   
-  const { data, status, update } = useSession();
+  const { data, status } = useSession();
   const user: any = data?.user
   const userId = user?.user_id;
   const userData = useUserData(userId);
-  const [isCorrect, setIsCorrect] = useState<'correct'| 'incorrect' | 'pending'>('pending')
-  const [lvlPosition, setLvlPosition] = useState<number>(0)
-  const [level, setLevel] = useState<number>(0)
-  const [formAnswer, setFormAnswer] = useState<string>('')
   
   // EStado pendiente
   if(status === 'loading') {
@@ -36,125 +25,20 @@ export const GameSection: React.FC<Props> = ({cat, lvl}) => {
   }
   // EStado usuario logueado
   if(status === 'authenticated' && data?.user) {
-    const user: any = userData
-    const catLevels = 
-    cat === 'movies' ? user?.user_datagame?.movies 
-    : cat === 'series' ? user?.user_datagame?.series 
-    : user?.user_datagame?.videogames
-    
-    const levelsListAvaliables = lvl?.filter(lvl => !catLevels?.some((userLvl:any) => userLvl.level_id === lvl.level_id)
-      )
-    
-    const totalClues = [
-      {clue: ''},
-      ...levelsListAvaliables[level].level_clue
-    ]
-
-    const handleNextClue = async () => {
-      if (lvlPosition === 4 )  return
-
-      setLvlPosition(lvlPosition + 1)
-    }
-
-    const handleNextLevel = async () => {
-      // if(level+2 > levelsListAvaliables.length) return
-      if (level + 2 >= levelsListAvaliables.length) return
-      const p = lvlPosition === 0 ? 5 
-      : lvlPosition === 1 ? 4 
-      : lvlPosition === 2 ? 3 
-      : lvlPosition === 3 ? 2 
-      : lvlPosition === 4 ? 1 
-      : 0
-      const levelPoints = {
-        level_correct: p === 0 ? false : true,
-        level_errors: lvlPosition + 1,
-        level_id: levelsListAvaliables[level].level_id,
-        level_points: p
-      }
-      cat === 'movies' ? user.user_datagame.movies.push(levelPoints) 
-        : cat === 'series' ? user.user_datagame.series.push(levelPoints)
-        : user.user_datagame.videogames.push(levelPoints)
-      // updateUser(userData)
-      await UpdateUserById(user.user_id, userData)
-      setLvlPosition(0)
-      setIsCorrect('pending')
-      setFormAnswer('')
-    }
-
-    const handleForm = (e:React.FormEvent<HTMLFormElement>): void => {
-      e.preventDefault()
-
-      const lvlCorrectAnswers = levelsListAvaliables[level].level_answers
-      function normalizeString(str: string) {
-        return str.toLowerCase().replace(/[^\w\s]/gi, '');
-      }
-      const isAnswerCorrect = lvlCorrectAnswers.some(
-        str => normalizeString(str) === normalizeString(formAnswer).trim()
-      )
-      // RESPUESTA ERRONEA
-      if(!isAnswerCorrect) {
-        // Comprueba que aun quedan pistas disponibles (no ha llegado a 5 pistas)
-        if (lvlPosition < 4 ) {
-          handleNextClue()
-          setFormAnswer('')
-          
-          return
-        }
-        // Tras 5 pistas, la resuelve como incorrecta
-        setIsCorrect('incorrect')
-
-        return
-      }
-
-      // RESPUESTA CORRECTA 
-      setIsCorrect('correct')
-      setFormAnswer('')
-    }
-    return (
-
-      <section className={styles.container}>
-        <CanvasGame 
-          image={levelsListAvaliables[level].level_images[lvlPosition].img} 
-          imageCorrect={levelsListAvaliables[level].image_correct}
-          isCorrect={isCorrect} 
-        />  
-
-        <PositionLevel 
-          clues={totalClues} 
-          isCorrect={isCorrect}
-          levelPosition={lvlPosition}
-        />
-        {
-          isCorrect === 'pending' ? (
-            <FormGame 
-            isCorrect={isCorrect}
-            handleNextClue={handleNextClue}
-            formAnswer={formAnswer}
-            setFormAnswer={setFormAnswer}
-            handleForm={handleForm}
-          />
-          )
-          : <button onClick={handleNextLevel}>Siguiente Nivel</button>
-        }
-        {
-          lvlPosition !== 0 && (
-            <section className={styles.container_clues}>
-            <h2 className={styles.title_clues}>Pistas:</h2>
-              <ClueGame
-                levelPosition={lvlPosition}
-                clues={totalClues}
-              />
-          </section>
-          )
-        }
-
   
-      </section>
+    return (
+      <LoggedUserGame 
+      listLevels={lvl}
+      cat={cat}
+      userData={userData}
+        />
     )
+    
   } else {
     // EStado SIN usuario logueado
+    const demoLevelsList = lvl.slice(0,5)
     return (
-      <h1>temporal Sin usuario</h1>
+      <DemoGame listLevels={demoLevelsList} />
     )
   }
 
