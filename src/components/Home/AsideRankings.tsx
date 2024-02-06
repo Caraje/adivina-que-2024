5d0ff08f6718785a14fa72d8bb72c783f2ef8ce1
Caraje <carlosajenjo@gmail.com>
2024-02-06 16:17:54 +0100

    pass data from database to component

--- src/components/Home/AsideRankings.tsx
+++ src/components/Home/AsideRankings.tsx
@@ -1,22 +1,29 @@
+'use client'
 import styles from '@/styles/Home/AsideRankings.module.css'
 import { RankCard } from './RankCard'
-import { rankList } from '@/utils/exampleRankUser'
+import { useUserList } from '@/hooks/usersList'
+
 
 const AsideRankings = () => {
+  const usersData:any = useUserList()
+  // const usersList: User | null = usersData?.user
+  if(!usersData) {
+    return <div>Cargando...</div>
+  }
   return (
     <aside className={styles.aside_home}>
       <h2>Top 10</h2>
       <RankCard 
-        cat='Cine'
-        list={rankList}
+        cat='movies'
+        list={usersData.user}
       />
       <RankCard 
-        cat='Series'
-        list={rankList}
+        cat='series'
+        list={usersData.user}
       />
       <RankCard 
-        cat='Videojuegos'
-        list={rankList}
+        cat='videogames'
+        list={usersData.user}
       />
     </aside>
   )
