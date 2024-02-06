7283c5d2550aad41e0be1e519a62109830ae5376
Caraje <carlosajenjo@gmail.com>
2024-02-06 16:16:58 +0100

    change props of categoryCard

--- src/components/Home/SelectCategory.tsx
+++ src/components/Home/SelectCategory.tsx
@@ -1,8 +1,19 @@
+'use client'
 import styles from '@/styles/Home/SelectCategory.module.css'
 import { CategoryCard } from './CategoryCard'
 import Link from 'next/link'
+import { useSession } from 'next-auth/react'
+import useUserData from '@/hooks/user'
 
 export const SelectCategory = () => {
+  const { data, status } = useSession()
+  const actualUser:any = data?.user
+  const userInfo: any = useUserData(actualUser?.user_id)
+  
+  
+  if (status === 'loading') {
+    return <div>Cargando...</div>
+  }
   return (
     <main className={styles.select_category}>
       <h1 className={styles.title}>Adivina que</h1>
@@ -10,34 +21,31 @@ export const SelectCategory = () => {
         <Link 
           href={'/movies'}
           className={styles.link}
-        >          <CategoryCard 
+        >          
+        <CategoryCard 
             category={'Cine'} 
             image={'./static_img/movies_horizotal.webp'}
-            points={999}
-            correct={999}
-            errors={999}
+            userDataLevels={userInfo?.user_datagame.movies}
           />
           </Link>
         <Link 
           href={'/series'}
           className={styles.link}
-        >          <CategoryCard 
+        >          
+        <CategoryCard 
             category={'Series'} 
             image={'./static_img/series_horizotal.webp'}
-            points={999}
-            correct={999}
-            errors={999}
+            userDataLevels={userInfo?.user_datagame.series}
           />
         </Link>
         <Link 
           href={'/videogames'}
           className={styles.link}
-        >          <CategoryCard 
+        >          
+        <CategoryCard 
             category={'Videojuegos'} 
             image={'./static_img/videogames_horizotal.webp'}
-            points={999}
-            correct={999}
-            errors={999}
+            userDataLevels={userInfo?.user_datagame.videogames}
           />
         </Link>
       </section>
