6008198cbc1293657d5786b608f9648df0461234
Caraje <carlosajenjo@gmail.com>
2024-02-06 16:15:23 +0100

    clean console logs

--- src/hooks/user.tsx
+++ src/hooks/user.tsx
@@ -1,28 +1,3 @@
-// const getUserById = async (id:string) => {
-  
-//   try {
-//     const response = await fetch(`http://localhost:3000/api/users/${id}`, {
-//       method: 'GET',
-//       headers: {
-//         'Content-type': 'application/json'
-//       }
-//     });
-
-//     if (response.ok) {
-//       const user = await response.json();
-
-//       return user.cleanUser;  // Retorna la respuesta del servidor
-//     } else {
-//       console.error('Error fetching user data:', response.status);
-//       return null;  // Retorna null o maneja el error según tu lógica
-//     }
-//   } catch (error) {
-//     console.error('Error fetching user data:', error);
-//     return null;  // Retorna null o maneja el error según tu lógica
-//   }
-// }
-
-
 import { getUserById } from '@/controllers/users';
 import { useSession } from 'next-auth/react';
 import { useEffect, useState } from 'react';
