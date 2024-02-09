5fa1dec632a2bf10c1935e57058e7aea1ce01f65
Caraje <carlosajenjo@gmail.com>
2024-02-09 19:08:40 +0100

    add new validators

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -6,8 +6,40 @@ import bcrypt from 'bcryptjs';
 
 export async function GET(req: NextRequest ) {
   const query = req.nextUrl.searchParams
+  const user_name: string | null = query.get('name')
   const user_email: string | null = query.get('email')
   const user_password: string | null = query.get('pass')
+
+  if( user_name && user_email) {
+    await connectDB()
+    const userFoundByName = await User.findOne({user_name})
+    const userFoundByMail = await User.findOne({user_email})
+
+    if(userFoundByName) return NextResponse.json({
+      ok: false,
+      message: 'Ya existe un usuario con ese nombre',
+      type: 1
+    }, {
+      status: 400
+    })
+
+    if(userFoundByMail) return NextResponse.json({
+      ok: false,
+      message: 'Ya existe un usuario con ese correo',
+      type: 2
+    }, {
+      status: 400
+    })
+    
+    return NextResponse.json({
+      ok: true,
+      message: 'No existen usuarios con ese nombre o correo',
+      type: 3
+    }, {
+      status: 400
+    })
+  }
+
   if( user_email || user_password) {
     await connectDB()
     const userFoundByMail = await User.findOne({user_email})
