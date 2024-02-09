85028aeef3bb7b8538cfe34ee41de07d11eacd34
Caraje <carlosajenjo@gmail.com>
2024-02-09 19:09:21 +0100

    fix get info for user

--- src/components/layout/UserButton.tsx
+++ src/components/layout/UserButton.tsx
@@ -5,55 +5,49 @@ import { useModalStore } from '@/store/modal-store'
 import { useSession } from 'next-auth/react'
 import { signOut } from "next-auth/react"
 import Link from 'next/link'
+import useUserData from '@/hooks/user'
+import { User } from '@/types/types'
 
 
 export const UserButton = () => {
   const { toggleModal } = useModalStore()
   const { data, status } = useSession()
   const user:any = data?.user
-  
-  const handleLogout = async () => {
-    signOut()
+  const userState: any = useUserData(user?.user_id)
+
+  if(status === 'loading') {
+    return (<div>Cargando...</div>)
   }
-  return (
+  if(status === 'authenticated' && data?.user) {
+    // console.log(usernew)
+    const handleLogout = async () => {
+      signOut()
+    }  
+  return(
     <section className={styles.user_menu}>
-    {/* TODO: Crear botones para ir a registro/login y boton para abrir menu de usuario */}
-    {
-      status === 'authenticated' ?
-        <Link 
-          href={`/user/${user.user_id}`}
-          className={styles.btn_user}
-          >
-          <img src={user.user_avatar} alt='imagen de usuario' width={50} height={50}/>
-          {user.user_name}
-        </Link>
-        : (
-        <button 
-          onClick={toggleModal}
-          className={styles.btn_user}
-        >
-          Login/Register <IconUser size={30} />
-        </button>) 
-    }
-    
-    {/* {
-      (status === "authenticated") 
-        ? <Link href='/api/auth/signout'>signout</Link>
-        : <Link href='/api/auth/signin'>SignIn</Link>
-    } */}
-    {
-      (status === "authenticated") &&
-        (
-        <button
-          onClick={handleLogout}
-          className={styles.btn_logout}
-        >
-          <IconLogout size={20}/>
-        </button>
-        )
-    }
+      <Link 
+        href={`/user/${userState?.user_id}`}
+        className={styles.btn_user}
+      >
+        <img src={userState?.user_avatar} alt='imagen de usuario' width={50} height={50}/>
+          {userState?.user_name}
+      </Link>
+      <button
+        onClick={handleLogout}
+        className={styles.btn_logout}
+      >
+        <IconLogout size={20}/>
+      </button>
+    </section>
+  )  
+  }
 
-
-  </section>
+  return (
+    <button 
+      onClick={toggleModal}
+      className={styles.btn_user}
+    >
+      Login/Register <IconUser size={30} />
+    </button>
   )
 }
