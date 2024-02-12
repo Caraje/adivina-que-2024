0964255b47e8c293e87eb0431520015a213f4715
Caraje <carlosajenjo@gmail.com>
2024-02-12 18:38:26 +0100

    add Delete user

--- src/components/user/UpdateProfileUser.tsx
+++ src/components/user/UpdateProfileUser.tsx
@@ -2,9 +2,10 @@ import { User } from '@/types/types'
 import styles from '@/styles/user/UpdateProfileUser.module.css'
 import { useState } from 'react'
 import { uploadToCloudinary } from '@/config/cloudinary'
-import { useSession } from 'next-auth/react'
-import { UpdateUserById } from '@/controllers/users'
-import useUserData from '@/hooks/user'
+import { useSession, signOut } from 'next-auth/react'
+import { UpdateUserById, deleteUser, updateUserPassword } from '@/controllers/users'
+// import { useRouter } from 'next/router';
+
 
 interface Props {
   user: User,
@@ -19,12 +20,17 @@ type FormValues = {
 }
 export const UpdateProfileUser: React.FC<Props> = ({user,isOpen, setIsOpen}) => {
   const { data, status, update } = useSession()
+
   const userActive: any  = data?.user
   const [isOpenPassword, setIsOpenPassword] = useState(false)
   const [userName, setUserName] = useState<string>(user.user_name)
   const [userEmail, setUserEmail] = useState(user.user_email)
   const [userAvatar, setUserAvatar] = useState(user.user_avatar)
   const [userNetworks, setUserNetworks] = useState(user.user_networks)
+  const [isError, setIsError] = useState<{type: number, msg: string}>({type: 0, msg: ''})
+  const [isUploadImage, setIsUploadImage] = useState(false)
+  const [isDelete, setIsDelete] = useState(false)
+
 
 
   if(status === 'loading') {
@@ -42,29 +48,55 @@ export const UpdateProfileUser: React.FC<Props> = ({user,isOpen, setIsOpen}) => 
       user_networks: userNetworks
     }
     const updatedUser = await UpdateUserById(userActive.user_id, newUser, user.user_name, user.user_email)
-    // if(updatedUser.ok) {
-    //   update(newUser)
-    //   setIsOpen(false)
-    //   return
-    // }
-    console.log({updatedUser})
+    if(!updatedUser.ok){
+      setIsError({
+        type: updatedUser.type,
+        msg: updatedUser.message
+      })
+      return
+    }
+    
+    update(newUser)
+    setIsOpen(false)
+
   }
-  const handleUpdatePassword = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleUpdatePassword = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     const form = event.currentTarget
     const formData = new FormData(form)
-    const pass: FormValues = Object.fromEntries(formData.entries()) as FormValues
+    const {newPass, newPassB, oldPass}: FormValues = Object.fromEntries(formData.entries()) as FormValues
+    // console.log(userActive.user_email)
+    const passUpdated = await updateUserPassword(userActive, userActive.user_id, oldPass, newPass, newPassB )
+    if(!passUpdated.ok) {
+      setIsError({ type: passUpdated.type, msg: passUpdated.msg})
+      return
+    }
+    setIsError({ type: 0, msg: ''})
   }
 
   const handleUploadImage = async (event: React.ChangeEvent<HTMLInputElement>) => {
     event.preventDefault()
+    setIsUploadImage(true)
     const imageFile = event.target.files?.[0]
     if (imageFile) {
       const imageData = await uploadToCloudinary(imageFile);      
       if (imageData) {
         setUserAvatar(imageData.url);
+        setIsUploadImage(false)
       }      
     }
+  }
+
+  const handleDeleteUser = async () => {
+    const isdeleted = await deleteUser(userActive.user_id)
+    if(!isdeleted.ok) return setIsError({
+      type: 20,
+      msg: 'No se ha podido borrar el usuario'
+    })
+    await signOut()
+    window.location.href = '/'
+
+    
   }
 
   return (
@@ -79,7 +111,13 @@ export const UpdateProfileUser: React.FC<Props> = ({user,isOpen, setIsOpen}) => 
         <section className={styles.form_user}>
           <header className={styles.form_user_avatar}>
             <label className={styles.form_user_avatar_label}>
-              <img src={userAvatar} alt={`Imagen de avatar del usuario ${user.user_name}`} width={150} height={150}/>
+              <img 
+                className={styles.avatar_image}
+                src={userAvatar} 
+                alt={`Imagen de avatar del usuario ${user.user_name}`} 
+                width={150} 
+                height={150}
+              />
               <input
                 type='file'
                 name='avatar'
@@ -95,6 +133,9 @@ export const UpdateProfileUser: React.FC<Props> = ({user,isOpen, setIsOpen}) => 
                 value={userName}
                 onChange={(event) => {setUserName(event.target.value)}}
               />
+              {
+                isError.type === 1 && <small>{isError.msg}</small>
+              }
             </label>
             <label className={styles.form_label}>
               Email: 
@@ -103,6 +144,9 @@ export const UpdateProfileUser: React.FC<Props> = ({user,isOpen, setIsOpen}) => 
               value={userEmail}
               onChange={(event) => {setUserEmail(event.target.value)}}
               />
+              {
+                isError.type === 2 && <small>{isError.msg}</small>
+              }
             </label>
             <div className={styles.form_button_group}>
               <button 
@@ -113,7 +157,8 @@ export const UpdateProfileUser: React.FC<Props> = ({user,isOpen, setIsOpen}) => 
                 Password
               </button>
               <button 
-              type='button'
+                type='button'
+                onClick={() => { setIsDelete(true)}}
                 className={styles.form_btn_delete}
               >
                 Eliminar
@@ -121,8 +166,27 @@ export const UpdateProfileUser: React.FC<Props> = ({user,isOpen, setIsOpen}) => 
             </div>
           </div>
         </section>
-        
-        
+        {
+          isDelete && (
+            <section>
+            De verdad quiere borrar el perfil?, perdera todo el progreso y el proceso no es reversible
+            <button 
+              type='button'
+              onClick={handleDeleteUser}
+              className={styles.form_btn_delete}
+            >
+              Eliminar
+            </button>
+            <button 
+              type='button'
+              onClick={() => { setIsDelete(false)}}
+              className={styles.form_btn_delete}
+            >
+              Cancelar
+            </button>
+          </section>
+          )
+        }
         <section className={styles.form_section}>
         {
           userNetworks.map((net) => (
@@ -145,7 +209,7 @@ export const UpdateProfileUser: React.FC<Props> = ({user,isOpen, setIsOpen}) => 
           ))
         }
         </section>
-          <button className={styles.form_btn_update_pass}>Actualizar Usuario</button>
+          <button disabled={isUploadImage} className={isUploadImage ? styles.form_btn_update_pass_disabled : styles.form_btn_update_pass}>Actualizar Usuario</button>
       </form>
       {
           isOpenPassword && (
@@ -159,6 +223,9 @@ export const UpdateProfileUser: React.FC<Props> = ({user,isOpen, setIsOpen}) => 
                   type='password'
                   name='oldPass'
                 />
+              {
+                isError.type === 10 || isError.type === 13 && <small>{isError.msg}</small>
+              }
               </label>
               <label className={styles.form_label}>
                 Nuevo Password:
@@ -166,6 +233,9 @@ export const UpdateProfileUser: React.FC<Props> = ({user,isOpen, setIsOpen}) => 
                   type='password'
                   name='newPass'
                 />
+              {
+                (isError.type === 10 || isError.type === 11 || isError.type === 12) && <small>{isError.msg}</small>
+              }
               </label>
               <label className={styles.form_label}>
                 Repita Password:
@@ -173,6 +243,9 @@ export const UpdateProfileUser: React.FC<Props> = ({user,isOpen, setIsOpen}) => 
                   type='password'
                   name='newPassB'
                 />
+              {
+                (isError.type === 10 || isError.type === 12) && <small>{isError.msg}</small>
+              }
               </label>
               <button 
                 className={styles.form_btn_update_pass}
